183690a2deb41ddbd1a54766e53aa0235c5163c2
inger-na <59580764+inger-na@users.noreply.github.com>
2022-01-08 22:08:50 +0100

    my solution

--- Solutions/between.js
+++ Solutions/between.js
@@ -1,1 +1,12 @@
+'use strict';
 
+const extractSubstring = (str, prefix, sufix) => {
+  const start = str.indexOf(prefix);
+  const end = str.lastIndexOf(sufix);
+  if (start === -1 && end === -1) return '';
+  const substring = str.substring(start + 1, end);
+  return substring;
+};
+
+const result = extractSubstring('Hello <username> and bye!', '<', '>');
+console.log(result);
